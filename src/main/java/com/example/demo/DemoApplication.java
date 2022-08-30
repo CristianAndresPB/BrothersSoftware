503bd854824e56e9e21db53da8c5d0bcbf5e5f57
Leidy Andrea Pérez Barbosa <leidyprz11@hotmail.com>
2022-08-30 11:18:59 -0500

    Se agrega 2 empresa de ejemplo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -37,6 +37,32 @@ public class DemoApplication {
 
 		System.out.println("El nombre de la empresa 1: " + empleado1.getEmpresa().getNombre());
 		System.out.println("El nombre del empleado 1: " + empleado1.getName());
+
+		Empresa empresa2 = new Empresa();
+		empresa2.setNombre("SysWorld SAS");
+		empresa2.setDireccion("Calle 127 #30-36");
+		empresa2.setNumeroEmpresa(321584883);
+		empresa2.setNitEmpresa(1234632);
+
+		Empleado empleado2 = new Empleado();
+		empleado2.setName("Leidy Perez");
+		empleado2.setEmail("leidyprz11@.hotmail.com");
+		empleado2.setEmpresa(empresa2);
+		empleado2.setRol("Administrador");
+
+		Movimientos movimiento3 = new Movimientos();
+		movimiento3.setMontoMovimiento(2345632);
+		movimiento3.setMontoNegativoyPosivitos(-12353);
+		movimiento3.setConcepto("Concepto del movimiento 3");
+		Movimientos movimiento4 = new Movimientos();
+		movimiento4.setMontoMovimiento(124672);
+		movimiento4.setMontoNegativoyPosivitos(1223452);
+		movimiento4.setConcepto("Concepto del movimiento 4");
+		Movimientos[] movimientos2 = new Movimientos[]{movimiento3,movimiento4};
+		empleado2.setMovimientos(movimientos2);
+
+		System.out.println("El nombre de la empresa 2: " + empleado2.getEmpresa().getNombre());
+		System.out.println("El nombre del empleado 2: " + empleado2.getName());
 	}
 
 }

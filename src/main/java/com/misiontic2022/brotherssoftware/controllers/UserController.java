1601a28d5dcf7221bd018595a015a3b8434e1554
CristianAndresPB <cristian.perezb1991@gmail.com>
2022-09-19 22:42:19 -0500

    Se Finaliza UserController

--- src/main/java/com/misiontic2022/brotherssoftware/controllers/UserController.java
+++ src/main/java/com/misiontic2022/brotherssoftware/controllers/UserController.java
@@ -1,14 +1,13 @@
 package com.misiontic2022.brotherssoftware.controllers;
 
+import com.misiontic2022.brotherssoftware.models.ObjectResponse;
 import com.misiontic2022.brotherssoftware.models.Usuario;
 import com.misiontic2022.brotherssoftware.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -26,7 +25,7 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<Object> getUser(@PathVariable String  id) {
+    public ResponseEntity<Object> getUser(@PathVariable String id) {
         try {
             Usuario usuario = userService.getUsuario(id);
             return new ResponseEntity<>(usuario, HttpStatus.OK);
@@ -43,5 +42,40 @@ public class UserController {
         } catch (Exception ex) {
             return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<ObjectResponse> postUser(@RequestBody Usuario usuario) {
+        return new ResponseEntity<>(
+                new ObjectResponse(userService.saveUser(usuario), "Usuario Creado Correctamente"),
+                HttpStatus.OK);
+    }
+
+    @PutMapping("/user")
+    public ObjectResponse putUpdateUser(@RequestBody Usuario usuario) {
+        return new ObjectResponse(userService.putUpdateUser(usuario), "Usuario Actualizado Correctamente");
+    }
+
+    @PatchMapping("/user/{id}")
+    public ResponseEntity<ObjectResponse> patchUpdateUser(@RequestBody Usuario usuario, @PathVariable String id) {
+        try {
+            return new ResponseEntity<>(
+                    new ObjectResponse(userService.patchUpdateUser(usuario, id), "Usuario Actualizado Correctamente"),
+                    HttpStatus.OK
+            );
+        } catch (Exception ex){
+            return new ResponseEntity<>(
+                    new ObjectResponse(null, ex.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<ObjectResponse> deleteUser(@PathVariable String id){
+        return new ResponseEntity<>(
+                new ObjectResponse(null, userService.deleteUser(id)),
+                HttpStatus.OK
+        );
     }
 }
--- src/main/java/com/misiontic2022/brotherssoftware/services/UserService.java
+++ src/main/java/com/misiontic2022/brotherssoftware/services/UserService.java
@@ -1,7 +1,9 @@
 package com.misiontic2022.brotherssoftware.services;
 
+import com.misiontic2022.brotherssoftware.models.ObjectResponse;
 import com.misiontic2022.brotherssoftware.models.Usuario;
 import com.misiontic2022.brotherssoftware.repository.UserRepository;
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +27,33 @@ public class UserService {
         } else {
             throw  new Exception("Usuario No Existe");
         }
+    }
+
+    public Usuario saveUser(Usuario usuario){
+        return userRepository.save(usuario);
+    }
+
+    public Usuario putUpdateUser(Usuario usuario){
+        return userRepository.save(usuario);
+    }
+
+    public Usuario patchUpdateUser(Usuario usuario, String id) throws Exception{
+        try {
+            Usuario usuarioDB = getUsuario(id);
+            if (usuario.getNombrePersona() != null){
+                usuarioDB.setNombrePersona(usuario.getNombrePersona());
+            }
+            if (usuario.getPassword() != null){
+                usuarioDB.setPassword(usuario.getPassword());
+            }
+            return saveUser(usuarioDB);
+        } catch (Exception ex) {
+            throw new Exception("Usuario no actualizado por que no existe");
+        }
+    }
+
+    public String deleteUser(String id){
+        userRepository.deleteById(id);
+        return "Usuario Eliminado Correctamente";
     }
 }
